49420008c41079d3dbd852903593e19c01516702
Josh Pollock <jpollock412@gmail.com>
2015-01-30 23:05:00 -0500

    intial commit

---
+++ src/main.php
@@ -0,0 +1,353 @@
+<?php
+/**
+ * Main class for processing license activations/deactivations
+ *
+ * @package calderawp\licensing
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2015 Josh Pollock
+ */
+
+/**
+ * @version 1.0.0
+ */
+
+namespace calderawp\licensing;
+
+/**
+ * Class main
+ *
+ * @package calderawp\licensing
+ */
+class main {
+
+	/**
+	 * The params passed to this class
+	 *
+	 * @var array
+	 */
+	protected $params;
+
+	/**
+	 * The name of the option that stores the license key.
+	 *
+	 * @var string
+	 */
+	public $license_key_option_name;
+
+	/**
+	 * The name of the option that stores the license status.
+	 *
+	 * @var string
+	 */
+	public $license_status_option_name;
+
+	/**
+	 * The AJAX action used for processing license codes.
+	 *
+	 * @var string
+	 */
+	public $ajax_action;
+
+	/**
+	 * The field containing the nonce when processing license codes.
+	 *
+	 * @var string
+	 */
+	public $nonce_field;
+
+	/**
+	 * The name of the action for the nonce.
+	 *
+	 * @var string
+	 */
+	public $nonce_action;
+
+	/**
+	 * Class constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $params {
+	 *
+	 *      @type string store_url URL for the store.
+	 *      @type string version Plugin version.
+	 *      @type string item_name The plugin's name.
+	 *      @type string author The plugin's author.
+	 *      @type string license_key_option_name The name of the option that stores the license key.
+	 *      @type string license_status_option_name The name of the option that stores the license status.
+	 *      @type string plugin_root_file The file path for the file containing the plugin header.
+	 *      @type string ajax_action The AJAX action used for processing license codes.
+	 *      @type string nonce_field The field containing the nonce when processing license codes.
+	 *      @type string nonce_action The name of the action for the nonce.
+	 *
+	 * }
+	 */
+	function __construct( $params ) {
+		$this->params = $params;
+		$this->nonce_action = $this->params[ 'nonce_action' ];
+		$this->nonce_field = $this->params[ 'nonce_field' ];
+		$this->license_key_option_name = $this->params[ 'license_key_option_name' ];
+		$this->license_status_option_name = $this->params[ 'license_status_option_name' ];
+
+		$ajax_action = 'wp_ajax_'.$params[ 'ajax_action' ];
+		add_action( $ajax_action, array( $this, 'update' ) );
+		add_action( 'admin_init', array( $this, 'register_license_key_option' ) );
+	}
+
+	/**
+	 * Initialize the EDD updater class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function updater_class(  ) {
+		if( ! class_exists( 'EDD_SL_Plugin_Updater' ) ) {
+
+			include( dirname( __FILE__ ) . '/includes/EDD_SL_Plugin_Updater.php' );
+		}
+
+		$license_key = trim( get_option( $this->params[ 'license_key_option_name' ] ) );
+
+		$edd_updater = new EDD_SL_Plugin_Updater( $this->params[ 'store_url' ], $this->params[ 'plugin_root_file' ], array(
+				'version' 	=> $this->params[ 'version' ],
+				'license' 	=> $license_key,
+				'item_name' => $this->params[ 'author' ],
+			)
+
+		);
+
+	}
+
+	/**
+	 * Process the license activation or deactivation
+	 *
+	 * @since 1.0.0
+	 */
+	public function update() {
+		$license_data = false;
+		$data = filter_input_array( INPUT_POST, FILTER_SANITIZE_STRING) ;
+
+		$error_response = array(
+			'success' => false,
+			'message' => __( 'License Could Not Be Processed. Please try again' ),
+			'license' => 'false'
+		);
+
+		if ( ! is_array( $data ) || empty( $data ) || ! wp_verify_nonce(  $data[ $this->params[ 'nonce_field' ] ], $this->params[ 'nonce_action' ] ) ){
+			wp_send_json_error( $error_response );
+		}
+
+		if ( isset( $data[ 'code'] ) ) {
+			$code = $data[ 'code' ];
+			if ( isset( $data[ 'license_action' ] ) ) {
+				if ( 'activate' === $data[ 'license_action' ]  ) {
+					$license_data = $this->activate_license( $code );
+				} else {
+					$license_data = $this->deactivate_license( $code );
+				}
+			}
+
+			nocache_headers();
+			if ( is_object( $license_data ) ) {
+
+				if ( isset( $license_data->success ) && $license_data->success ) {
+					$license_data = (array) $license_data;
+					$license_data[ 'license' ] = 'activated';
+					wp_send_json_success( $license_data  );
+				}elseif( isset( $license_data->license ) && 'deactivated' == $license_data->license ) {
+					$license_data = (array) $license_data;
+					$license_data[ 'license' ] = 'deactivated';
+					wp_send_json_success( $license_data );
+				}
+				else{
+					wp_send_json_error(  $license_data );
+				}
+
+			}
+
+			wp_send_json_error( $error_response );
+
+		}
+
+	}
+
+	/**
+	 * Activate License
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 *
+	 * @param string|bool $license Optional. The license code. If false, will get from DB.
+	 *
+	 * @return bool|void
+	 */
+	protected function activate_license( $license = false ) {
+		$response = $this->api_request( 'activate_license', $license );
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+		if ( isset( $license_data->success ) && $license_data->success ) {
+			update_option( $this->params['license_key_option_name'], $license );
+			update_option( $this->params['license_status_option_name'], true );
+		}else{
+			return false;
+
+		}
+
+		return $license_data;
+
+	}
+
+	/**
+	 * Deactivate license
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 *
+	 * @param string|bool $license Optional. The license code. If false, will get from DB.
+	 *
+	 * @return bool|void
+	 */
+	protected function deactivate_license( $license = false ) {
+		$response = $this->api_request( 'deactivate_license', $license );
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+
+		if( isset( $license_data->license ) && $license_data->license == 'deactivated' ) {
+			delete_option( $this->params[ 'license_key_option_name' ] );
+			delete_option( $this->params[ 'license_status_option_name' ] );
+		}
+		else{
+			return false;
+		}
+
+		return $license_data;
+
+	}
+
+
+	/**
+	 * Register option for the license key
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 *
+	 * @uses 'admin_init' action
+	 */
+	public function register_license_key_option() {
+		register_setting( 'cep_licenses', $this->params[ 'license_key_option_name' ], array( $this, 'sanitize_license' ) );
+	}
+
+
+	/**
+	 * Sanitization callback for register_setting() used in $this->register_license_key_option()
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $new The new license key.
+	 *
+	 * @return mixed|bool|string
+	 */
+	public function sanitize_license( $new ) {
+		$old = get_option( $this->params[ 'license_key_option_name' ] );
+		if( $old && $old != $new ) {
+			delete_option( $this->params[ 'license_status_option_name' ] );
+		}
+
+		return $new;
+
+	}
+
+	/**
+	 * Check a license.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 *
+	 * @param string|bool $license Optional. The license code. If false, will get from DB.
+	 *
+	 * @return bool
+	 */
+	protected function check_license( $license = false ) {
+
+		global $wp_version;
+
+		$response = $this->api_request( 'check_license', $license );
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+
+		}
+
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+		if( isset( $license_data->license ) && $license_data->license == 'valid' ) {
+			echo 'valid';
+			exit;
+
+		} else {
+			echo 'invalid';
+			exit;
+
+		}
+
+	}
+
+	/**
+	 * Make request to licensing API
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $edd_action Action to take on remote server activate|deactivate|check_license
+	 * @param string|bool $license Optional. The license code. If false, will get from DB.
+	 *
+	 * @return array|\WP_Error
+	 */
+	protected function api_request( $edd_action, $license = false ) {
+		if ( ! in_array( $edd_action, array( 'activate', 'deactivate', 'check_license' ) ) )  {
+			return false;
+		}
+
+		if ( ! $license ) {
+			$license = get_option( $this->params[ 'license_key_option_name' ] );
+		}
+
+		$license = trim( $license );
+
+
+		$api_params = array(
+			'edd_action'=> $edd_action,
+			'license' 	=> $license,
+			'item_name' => urlencode( $this->params[ 'item_name' ] ),
+			'url'       => home_url()
+		);
+
+		$url      = add_query_arg( $api_params, $this->params[ 'store_url' ] );
+		$response = wp_remote_get(
+			$url, array(
+				'timeout'   => 15,
+				'sslverify' => false
+			)
+		);
+
+		return $response;
+
+	}
+
+}
---
+++ src/output.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Functions to be used in building licensing UI
+ *
+ * @package   calderawp\licensing
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2015 Josh Pollock
+ */
+
+/**
+ * @version 1.0.0
+ */
+namespace calderawp\licensing;
+
+/**
+ * Class output
+ *
+ * @package calderawp\licensing
+ */
+class output {
+
+	/**
+	 * Object of the main licensing class
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var \calderawp\licensing\main|object
+	 */
+	public $main_licensing_class;
+
+	/**
+	 * Arguments for a BaldrickJS button
+	 *
+	 * @var array
+	 */
+	protected $button_baldrick;
+
+	/**
+	 * Constructor for this class.
+	 *
+	 * @param \calderawp\licensing\main|object $main_licensing_class The related instance of the main licensing class.
+	 */
+	function __construct( $main_licensing_class, $button_baldrick ) {
+		$this->button_baldrick = $button_baldrick;
+		$this->main_licensing_class = $main_licensing_class;
+	}
+
+	/**
+	 * Get current, saved license code.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string|bool The code or false if not set.
+	 */
+	public function get_license_code() {
+		$code = get_option( $this->main_licensing_class->license_key_option_name, false );
+
+		return $code;
+
+	}
+
+	/**
+	 * Get status of license code
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string|bool Status of code
+	 */
+	public function get_license_status() {
+		$status = get_option( $this->main_licensing_class->license_status_option_name, false );
+
+		return $status;
+	}
+
+	/**
+	 * Output a BaldrickJS submit button for the activate/deactivate action
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string Button HTML
+	 */
+	public function submit_button() {
+
+		if ( ! $this->is_license_valid() ) {
+			$license_action = 'activate';
+			$value = __( 'Activate License', 'easy-pods' );
+		}else{
+			$license_action = 'deactivate';
+			$value = __( 'Deactivate License', 'easy-pods' );
+		}
+
+		return $this->button_html( $license_action, $value );
+
+
+	}
+
+	/**
+	 * Actually constructs the button HTML
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 *
+	 * @param string $license_action The action to take activate|deactivate
+	 * @param string $value The value attribue for the button.
+	 *
+	 * @return string Button HTML
+	 */
+	protected function button_html( $license_action, $value ) {
+		$nonce_field = $this->nonce_field();
+		$atts = $this->button_baldrick;
+		$atts[ 'code' ] = 'false';
+		$atts[ $this->main_licensing_class->nonce_field ] = 'false';
+		$atts[ 'license_action' ] = $license_action;
+
+		foreach( $atts as $att => $value ) {
+			if ( is_string( $value ) ) {
+				$atts_html[] = 'data-' . $att .'=' . esc_attr( $value );
+			}
+		}
+
+		$atts_html = implode( ' ', $atts_html );
+
+
+		return sprintf( '%1s<input type="submit" id="code-submit" class="wp-baldrick add-new-h2"  %2s value="%4s"/>',
+			$nonce_field,
+			esc_attr( $atts_html ),
+			ucfirst( $value ) );
+
+
+	}
+
+	/**
+	 * Check if the stored license is valid.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	public function is_license_valid() {
+		$status  = get_option( $this->main_licensing_class->license_status_option_name, false );
+		if ( false == $status || 'invalid' == $status ) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Create a nonce field.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string Form field HTML
+	 */
+	public function nonce_field() {
+		$nonce_field = wp_nonce_field( $this->main_licensing_class->nonce_field, $this->main_licensing_class->nonce_action, true, false );
+
+		return $nonce_field;
+
+	}
+
+}
