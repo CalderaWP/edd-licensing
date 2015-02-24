f7a3ad7dc8ce0e497d1599ab59e262c6d10e30ac
Josh Pollock <jpollock412@gmail.com>
2015-02-23 20:55:12 -0500

    move most of output into an abstract class and create second option

--- src/output.php
+++ src/output.php
@@ -19,16 +19,7 @@ namespace calderawp\licensing;
  *
  * @package calderawp\licensing
  */
-class output {
-
-	/**
-	 * Object of the main licensing class
-	 *
-	 * @since 1.0.0
-	 *
-	 * @var \calderawp\licensing\main|object
-	 */
-	public $main_licensing_class;
+class output extends output_root {
 
 	/**
 	 * Arguments for a BaldrickJS button
@@ -47,32 +38,6 @@ class output {
 		$this->main_licensing_class = $main_licensing_class;
 	}
 
-	/**
-	 * Get current, saved license code.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return string|bool The code or false if not set.
-	 */
-	public function get_license_code() {
-		$code = get_option( $this->main_licensing_class->license_key_option_name, false );
-
-		return $code;
-
-	}
-
-	/**
-	 * Get status of license code
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return string|bool Status of code
-	 */
-	public function get_license_status() {
-		$status = get_option( $this->main_licensing_class->license_status_option_name, false );
-
-		return $status;
-	}
 
 	/**
 	 * Output a BaldrickJS submit button for the activate/deactivate action
@@ -134,35 +99,5 @@ class output {
 
 	}
 
-	/**
-	 * Check if the stored license is valid.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool
-	 */
-	public function is_license_valid() {
-		$status  = get_option( $this->main_licensing_class->license_status_option_name, false );
-		if ( false == $status || 'invalid' == $status ) {
-			return false;
-		}
-
-		return true;
-
-	}
-
-	/**
-	 * Create a nonce field.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return string Form field HTML
-	 */
-	public function nonce_field() {
-		$nonce_field = wp_nonce_field( $this->main_licensing_class->nonce_field, $this->main_licensing_class->nonce_action, true, false );
-
-		return $nonce_field;
-
-	}
 
 }
